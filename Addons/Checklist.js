b275e8b79bb7f1511a0f5fc94787234046f5d0e8
開飛機のzm <154767397+Bilibilizm@users.noreply.github.com>
2025-01-24 08:58:19 +0800

    Update Checklist.js

--- Addons/Checklist.js
+++ Addons/Checklist.js
@@ -1,15 +1,4 @@
 
-// ==UserScript==
-// @name         GeoFS Flight Inspection Checklist
-// @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  Thanks for useing.Good Flight!
-// @author       zm
-// @match        https://www.geo-fs.com/geofs.php?v=3.9
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
-// @grant        none
-// ==/UserScript==
-
 (function () {
     'use strict';
 
