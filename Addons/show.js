8111b2e8c98e742aa0671486bd904cc56b21c0e9
開飛機のzm <154767397+Bilibilizm@users.noreply.github.com>
2025-01-27 22:15:41 +0800

    Update show.js

--- Addons/show.js
+++ Addons/show.js
@@ -44,7 +44,7 @@
         menu.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.5)";
         menu.style.fontSize = "14px";
         menu.innerHTML = `
-            <h3 style="margin: 0; font-size: 16px; text-align: center;">GeoFS飞行状态显示器</h3>
+            <h3 style="margin: 0; font-size: 16px; text-align: center;">【W】GeoFS飞行状态显示器</h3>
             <p style="margin: 0; font-size: 12px; text-align: center; color: #ccc;">作者：bilibili-蜂蜜水的冬日航线123</p>
             <hr style="margin: 5px 0;">
             <div>
